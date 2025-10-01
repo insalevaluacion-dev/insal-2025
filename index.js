902c11ed3587eeb75957d5675ecfc15d687bf946
bapatata688 <b0ldm6diego@gmail.com>
2025-10-01 10:02:22 -0400

    fixed connection:)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 const conexion = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: parseInt(process.env.MYSQL_PORT || '3306'),
   waitForConnections: true,
