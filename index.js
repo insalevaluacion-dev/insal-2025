7421277f8d8be0d95f4335e669c175af1ff769f8
bapatata688 <b0ldm6diego@gmail.com>
2025-10-01 11:52:47 -0400

    added animations i think 2:)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const path = require('path');
 const session = require('express-session');
 const app = express();
